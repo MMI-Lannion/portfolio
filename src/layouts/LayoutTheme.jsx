bb84d5b3db3b8efd2d1e12453b68f52d0eb3ec4e
theoherv <theo.dancingdead@gmail.com>
2024-10-15 10:25:10 +0200

    Radix layout Theme
    - add LayoutTheme.jsx
    - Radix integration work

---
+++ src/layouts/LayoutTheme.jsx
@@ -0,0 +1,9 @@
+import * as Tabs from '@radix-ui/react-tabs';
+
+export default function LayoutTheme() {
+  return (
+    <Tabs.Root defaultValue="tab1">
+        <h1>Hello World!</h1>
+    </Tabs.Root>
+  );
+}
