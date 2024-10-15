e688c61bdf0622ba09eac8c070202a1e21fe07bb
Théo <theolg36@gmail.com>
2024-10-15 11:55:11 +0200

    En cours: plot graph radar

--- src/PlotFigure.js
+++
@@ -1,6 +0,0 @@
-import * as Plot from "@observablehq/plot";
-import {createElement as h} from "react";
-
-export default function PlotFigure({options}) {
-  return Plot.plot({...options, document: new Document()}).toHyperScript();
-}
--- src/components/plot/Radar.jsx
+++ src/components/plot/Radar.jsx
@@ -1,13 +1,40 @@
-import React from 'react';
 import * as Plot from "@observablehq/plot";
+import PlotFigure from "./PlotFigure.js";
 
-const Radar = () => {
+const penguins = [
+  {
+    "species": "Adelie",
+    "island": "Torgersen",
+    "culmen_length_mm": 39.1,
+    "culmen_depth_mm": 18.7,
+    "flipper_length_mm": 181,
+    "body_mass_g": 3750,
+    "sex": "MALE"
+  },
+  {
+    "species": "Adelie",
+    "island": "Torgersen",
+    "culmen_length_mm": 39.5,
+    "culmen_depth_mm": 17.4,
+    "flipper_length_mm": 186,
+    "body_mass_g": 3800,
+    "sex": "FEMALE"
+  }
+];
+
+export default function Radar() {
+
   return (
-    
-    <div>
-      
-    </div>
+    <>
+      <h1>Plot + React</h1>
+      <h2>Penguins scatterplot</h2>
+      <PlotFigure
+        options={{
+          marks: [
+            Plot.dot(penguins, { x: "culmen_length_mm", y: "culmen_depth_mm" })
+          ]
+        }}
+      />
+    </>
   );
-};
-
-export default Radar;
+}
