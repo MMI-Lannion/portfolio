50b8f01c2962b375624eebafd6805deec7225481
Mohamed Ezzaouia <ezzaouia@users.noreply.github.com>
2024-10-15 17:30:25 +0200

    clean up

--- src/components/AutoEvaluation.jsx
+++ src/components/AutoEvaluation.jsx
@@ -1,37 +1,52 @@
 // AutoEvaluation.
 
-import React from 'react';
-import { Radar } from './Radar';
-import { Flex, Text } from '@radix-ui/themes';
+import React from "react";
+import { Radar } from "./Radar";
+import { Flex, Text, Heading } from "@radix-ui/themes";
 
 export default function AutoEvaluation() {
+  // datas to send to radars
+  const datasRadar1 = {};
+  const datasRadar2 = {};
+  const datasRadar3 = {};
 
-    // datas to send to radars
-    const datasRadar1 = {};
-    const datasRadar2 = {};
-    const datasRadar3 = {};
+  return (
+    <Flex gap="4" direction="column">
+      <Flex gap="1" direction="column">
+        <Heading as="h3" size="3">
+          Hardskills :
+        </Heading>
+        <Text>
+          Estimez votre niveau de maitrise pour chaque sous compétence sur une
+          échelle de 1 à 5 (1 = Pas du tout maitrisé ; 5 = Tout à fait maitrisé)
+        </Text>
 
-    return (
-        <Flex gap="4" direction="column">
-            <Flex>
-                <Text size="4">Sous-titre</Text >
-            </Flex>
-            <Flex gap="2" direction="column">
-                <Text size="4">Titre</Text >
-                <Text size="2">Texte</Text >
-                <Radar datasRadar1={datasRadar1}/>
-            </Flex>
-            <Flex gap="2" direction="column">
-                <Text >Logoden biniou degemer mat, an. Kelenn itron kambr mar, houarn. Ac’hanout perak hent kaout, pediñ. Verc’h aon ennon prenañ, kurun. Eil honnezh vuoc’h beajourien, doujañs. Panevet  kemmañ den gontell, ar. Skrijañ c’hleñved vuhez gwrierez, kerf. Tre koan lien gwaskañ, Pleiber-Krist. Penn anavezout doñjer frout, houad. Evezh tregont gwriat huñvre, Mederieg.</Text >
-                <Text >Logoden biniou degemer mat, an. Kelenn itron kambr mar, houarn. Ac’hanout perak hent kaout, pediñ. Verc’h aon ennon prenañ, kurun. Eil honnezh vuoc’h beajourien, doujañs. Panevet  kemmañ den gontell, ar. Skrijañ c’hleñved vuhez gwrierez, kerf. Tre koan lien gwaskañ, Pleiber-Krist. Penn anavezout doñjer frout, houad. Evezh tregont gwriat huñvre, Mederieg.</Text >
-                <Radar datasRadar2={datasRadar2}/>
-            </Flex>
-            <Flex gap="2" direction="column">
-                <Text >Logoden biniou degemer mat, an. Kelenn itron kambr mar, houarn. Ac’hanout perak hent kaout, pediñ. Verc’h aon ennon prenañ, kurun. Eil honnezh vuoc’h beajourien, doujañs. Panevet  kemmañ den gontell, ar. Skrijañ c’hleñved vuhez gwrierez, kerf. Tre koan lien gwaskañ, Pleiber-Krist. Penn anavezout doñjer frout, houad. Evezh tregont gwriat huñvre, Mederieg.</Text >
-                <Text >Logoden biniou degemer mat, an. Kelenn itron kambr mar, houarn. Ac’hanout perak hent kaout, pediñ. Verc’h aon ennon prenañ, kurun. Eil honnezh vuoc’h beajourien, doujañs. Panevet  kemmañ den gontell, ar. Skrijañ c’hleñved vuhez gwrierez, kerf. Tre koan lien gwaskañ, Pleiber-Krist. Penn anavezout doñjer frout, houad. Evezh tregont gwriat huñvre, Mederieg.</Text >
-                <Radar datasRadar3={datasRadar3}/>
-            </Flex>
-        </Flex>
-    );
-  }
-  
+        <Radar datasRadar1={datasRadar1} />
+      </Flex>
+
+      <Flex gap="1" direction="column">
+        <Heading as="h3" size="3">
+          Softskills :
+        </Heading>
+        <Text>
+          Identifiez le ou les softskills que vous avez utilisé lors de votre
+          SAE ou de votre projet en ressource
+        </Text>
+
+        <Radar datasRadar1={datasRadar1} />
+      </Flex>
+
+      <Flex gap="1" direction="column">
+        <Heading as="h3" size="3">
+          Outils et méthodes :
+        </Heading>
+        <Text>
+          Estimer votre niveau de maitrise pour chaque outil et méthode sur une
+          échelle de 1 à 5 (1 = Pas du tout maitrisé ; 5 = Tout à fait maitrisé)
+        </Text>
+
+        <Radar datasRadar1={datasRadar1} />
+      </Flex>
+    </Flex>
+  );
+}
