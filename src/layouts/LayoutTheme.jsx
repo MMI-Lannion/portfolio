1dbe175954626e05bafad6fc16863ef5e44606ac
theoherv <theo.dancingdead@gmail.com>
2024-10-15 16:40:18 +0200

    Header & Footer V1

--- src/layouts/LayoutTheme.jsx
+++ src/layouts/LayoutTheme.jsx
@@ -1,7 +1,8 @@
 import Themes from "./Themes";
 import { Header } from "@/components/Header";
-// import { Footer } from "@/components/Footer";
+import { Footer } from "@/components/Footer";
 import { Box, Separator } from "@radix-ui/themes";
+
 
 export function LayoutTheme({ children }) {
   return (
@@ -11,6 +12,7 @@ export function LayoutTheme({ children }) {
       </Box>
       <Box m="4">{children}</Box>
       {/* <Footer /> */}
+      <Footer />
     </Themes>
   );
 }
