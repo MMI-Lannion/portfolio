6b2c5cc8a9f4a2ccd26c4aa67ad9392d1833ceb5
Mohamed Ezzaouia <ezzaouia@users.noreply.github.com>
2024-10-21 17:45:39 +0200

    wip

--- src/components/AutoEvaluation.jsx
+++ src/components/AutoEvaluation.jsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import { Radar } from "./Radar";
-import { Flex, Text, Heading, Box } from "@radix-ui/themes";
+import { Flex, Text, Heading, Box, Tabs } from "@radix-ui/themes";
 import { SunburstChart } from "./SunburstChart.tsx";
 
 const test1 = [
@@ -44,7 +44,30 @@ const test3 = [
 export function HardskillsRadar() {
   return (
     <Flex justify="center">
-      <Radar datas={test1} />
+      <Tabs.Root defaultValue="account">
+        <Tabs.List justify="start">
+          <Tabs.Trigger value="account">Account</Tabs.Trigger>
+          <Tabs.Trigger value="documents">Documents</Tabs.Trigger>
+          <Tabs.Trigger value="settings">Settings</Tabs.Trigger>
+        </Tabs.List>
+
+        <Box pt="3">
+          <Tabs.Content value="account">
+            <Text size="2">Make changes to your account.</Text>
+            <Radar datas={test1} />
+          </Tabs.Content>
+
+          <Tabs.Content value="documents">
+            <Text size="2">Access and update your documents.</Text>
+          </Tabs.Content>
+
+          <Tabs.Content value="settings">
+            <Text size="2">
+              Edit your profile or update contact information.
+            </Text>
+          </Tabs.Content>
+        </Box>
+      </Tabs.Root>
     </Flex>
   );
 }
